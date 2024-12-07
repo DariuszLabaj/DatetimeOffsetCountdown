988327a2175a6774aac94307a320309fa32e0783
DarekLabaj <dareklabaj@gmail.com>
2024-12-07 17:41:32 +0100

    fix 2

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from src import MainWindow
 import os
-from py_resource_manager import CultureInfo, ResourceManager
+from py_resource_manager import ResourceManager
 ResourceManager(os.path.dirname(os.path.abspath(__file__)) + "/Resources/Strings")
+from src import MainWindow
 # from timezones import TimeZones
 
 
 if __name__ == "__main__":
-    print(CultureInfo.CurrentCulture)
     mainViewModel = MainWindow(1024, 768)
     # mainViewModel.SetTimezoneOffset(TimeZones.Central_Europe)  # Set timezone manually if automatic one does not work for some reason
     # mainViewModel.SetDebug()

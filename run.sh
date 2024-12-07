eab6ec2749a8f0f6066e135f6a94445314d75f0f
DariuszLabaj <75167488+DariuszLabaj@users.noreply.github.com>
2024-12-07 16:14:38 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Change directory to the project directory
-cd /home/EolTester1!/WorkSpaces/DatetimeOffsetCountdown
-
-# Run the Python script using the virtual environment
-/home/EolTester1!/WorkSpaces/DatetimeOffsetCountdown/.venv/bin/python main.py
